f90170c86205403a62bf96c329580bb0ad454eed
Igor Osadchyi <igor@webio.net.ua>
2019-06-08 14:06:06 +0300

    Updates

---
+++ precache-manifest.06ad6f32451b5c467a292227eb495e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed10ccf39b4479d93f9e9a02308275ad",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "05ac51ae9ef17f3f2187",
+    "url": "/Random-Quote-Machine/static/css/main.06819e59.chunk.css"
+  },
+  {
+    "revision": "824ee409ce82a9c2a456",
+    "url": "/Random-Quote-Machine/static/js/2.284debaa.chunk.js"
+  },
+  {
+    "revision": "05ac51ae9ef17f3f2187",
+    "url": "/Random-Quote-Machine/static/js/main.c35acba1.chunk.js"
+  },
+  {
+    "revision": "fc1abb5e9f8eb11e4c02",
+    "url": "/Random-Quote-Machine/static/js/runtime~main.6010cb3b.js"
+  }
+]);
